94f22cb46dc7d1d5052a16d27ea62166f2329b16
ayahkhalilia <ayah.khalilia@stu.shenkar.ac.il>
2025-06-25 23:23:36 +0300

    added the task schema in models

--- server/models/Task.js
+++ server/models/Task.js
@@ -1,10 +1,6 @@
-// Require mongoose
-
-
-// Implement schema for a task
-// Each task should have:
-// - title (String, required)
-// - done (Boolean, defaults to false)
-
-
-// Export the model as 'Task'
+const mongoose=require('mongoose');
+const taskSchema=new mongoose.Schema({
+    title:{type:String,require:true},
+    done:{type:Boolean,default:false}
+});
+module.exports=mongoose.model('task',taskSchema);
